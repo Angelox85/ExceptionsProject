e1102a63f796f770ccd13621d8946290b741ec7f
Angel Hernandez Sierra <angel.hernandezsierr@sinclair.edu>
2021-04-17 20:13:02 -0400

    Definition of prototypes

--- Source.cpp
+++ Source.cpp
@@ -13,5 +13,6 @@ using namespace std;
 // Function prototype
 char character(char, int);
 bool isCharacter(char);
+// define enum to determine if letter is upper case, lower cae, or if is not a letter
 enum CharCaseRange { upperCase, lowerCase, outOfRange };
 CharCaseRange resolveChactCaseRange(char);
